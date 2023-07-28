ab188b1b7bc13a7ba391114d196b3df6651f7e22
batuhan <batusalkim11@gmail.com>
2023-07-28 03:37:59 +0300

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,27 +4,28 @@ import {FaFacebookF,FaTwitter,FaGooglePlusG,FaInstagram,FaBars} from "react-icon
 
 function Navbar() {
   return (
-    <div>
-        <ul>
+    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80'>
+        <ul className='hidden sm:flex px-4'>
             <li><a href="/">Home</a></li>
             <li><a href="#gallery">Gallery</a></li>
             <li><a href="#deals">Deals</a></li>
             <li><a href="#contact">Contact</a></li>
         </ul>
-        <div>
+        <div className='flex justify-between'>
               <FaFacebookF className="mx-4"/>
               <FaTwitter className="mx-4" />
               <FaGooglePlusG className="mx-4" />
               <FaInstagram className="mx-4" />
         </div>
-        <div>
+        <div className='sm:hidden z-10'>
             <FaBars/>
         </div>
         <div>
             <ul>
                 <li><a href="/">Home</a></li>
-                <li><a href="#gallery">Home</a></li>
-                <li><a href="#deals">Home</a></li>
+                <li><a href="#gallery">Gallery</a></li>
+                <li><a href="#deals">Deals</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </div>
     </div>
