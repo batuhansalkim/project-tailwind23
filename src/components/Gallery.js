e79348f6efb18338aee7a252c0d5f01dc6fc667d
batuhan <batusalkim11@gmail.com>
2023-07-30 21:01:01 +0300

    update

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,21 +4,21 @@ function Gallery() {
   return (
     <div id='gallery' className='max-w-[1140px] m-auto w-full px-4 py-16'>
         <h2 className='text-center text-gray-700 p-4'>Gallery</h2>
-        <div>
-            <div>
-                <img src="https://images.unsplash.com/photo-1683009427666-340595e57e43?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="/" />
+        <div className='grid sm:grid-cols-5 gap-4'>
+            <div className='sm:col-span-3 col-span-2 row-span-2'>
+                <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1683009427666-340595e57e43?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="/" />
             </div>
             <div>
-                <img src="https://images.unsplash.com/photo-1525275963076-7a70249a9925?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80" alt="/" />
+                <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1525275963076-7a70249a9925?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1172&q=80" alt="/" />
             </div>
             <div>
-                <img src="https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="/" />
+                <img className='w-full h-full object-cover' src="https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="/" />
             </div>
             <div>
-                <img src="https://images.pexels.com/photos/17636977/pexels-photo-17636977/free-photo-of-skyscrapers-in-new-york.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+                <img className='w-full h-full object-cover' src="https://images.pexels.com/photos/17636977/pexels-photo-17636977/free-photo-of-skyscrapers-in-new-york.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
             </div>
             <div>
-                <img src="https://images.pexels.com/photos/17626710/pexels-photo-17626710/free-photo-of-wood-light-road-dawn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="/" />
+                <img className='w-full h-full object-cover' src="https://images.pexels.com/photos/17626710/pexels-photo-17626710/free-photo-of-wood-light-road-dawn.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="/" />
             </div>
         </div>
     </div>
